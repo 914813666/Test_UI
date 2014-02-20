43c23ddc918b1010a905ccf75a10904dd18043a3
QZP <rimie17@rimie17deiMac.local>
2014-02-20 11:59:41 +0800

    Initial Commit

---
+++ Test_UI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test_UI
+//
+//  Created by QZP on 14-2-20.
+//  Copyright (c) 2014年 QZP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test_UITests/Test_UITests.m
@@ -0,0 +1,34 @@
+//
+//  Test_UITests.m
+//  Test_UITests
+//
+//  Created by QZP on 14-2-20.
+//  Copyright (c) 2014年 QZP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_UITests : XCTestCase
+
+@end
+
+@implementation Test_UITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
